18d5958a82fbc19f74fa56efa87ec42bc9c5f8ae
Rami Zouari <zouari.rami@yahoo.com>
2026-03-04 12:49:52 +0100

    Add: Support for Neural ODE (Direct only)

--- torchdiffeq/_impl/solvers.py
+++ torchdiffeq/_impl/solvers.py
@@ -26,7 +26,9 @@ class AdaptiveStepsizeODESolver(metaclass=abc.ABCMeta):
         return set()
 
     def integrate(self, t):
-        solution = torch.empty(len(t), *self.y0.shape, dtype=self.y0.dtype, device=self.y0.device)
+        solution = torch.empty(
+            len(t), *self.y0.shape, dtype=self.y0.dtype, device=self.y0.device
+        )
         solution[0] = self.y0
         t = t.to(self.dtype)
         self._before_integrate(t)
@@ -36,7 +38,6 @@ class AdaptiveStepsizeODESolver(metaclass=abc.ABCMeta):
 
 
 class AdaptiveStepsizeEventODESolver(AdaptiveStepsizeODESolver, metaclass=abc.ABCMeta):
-
     @abc.abstractmethod
     def _advance_until_event(self, event_fn):
         raise NotImplementedError
@@ -52,10 +53,22 @@ class AdaptiveStepsizeEventODESolver(AdaptiveStepsizeODESolver, metaclass=abc.AB
 class FixedGridODESolver(metaclass=abc.ABCMeta):
     order: int
 
-    def __init__(self, func, y0, step_size=None, grid_constructor=None, interp="linear", perturb=False, **unused_kwargs):
-        self.atol = unused_kwargs.pop('atol')
-        unused_kwargs.pop('rtol', None)
-        unused_kwargs.pop('norm', None)
+    def __init__(
+            self,
+            func,
+            y0,
+            step_size=None,
+            grid_constructor=None,
+            interp="linear",
+            perturb=False,
+            jump_t=None,
+            events=None,
+            jump=None,
+            **unused_kwargs,
+    ):
+        self.atol = unused_kwargs.pop("atol")
+        unused_kwargs.pop("rtol", None)
+        unused_kwargs.pop("norm", None)
         _handle_unused_kwargs(self, unused_kwargs)
         del unused_kwargs
 
@@ -66,6 +79,17 @@ class FixedGridODESolver(metaclass=abc.ABCMeta):
         self.step_size = step_size
         self.interp = interp
         self.perturb = perturb
+        self.jump_t = (
+            torch.as_tensor(jump_t, dtype=self.dtype, device=self.device)
+            if jump_t is not None
+            else torch.tensor([], dtype=self.dtype, device=self.device)
+        )
+        self.jump = jump
+        self.events = (
+            torch.as_tensor(events, dtype=self.y0.dtype, device=self.device)
+            if events is not None
+            else None
+        )
 
         if step_size is None:
             if grid_constructor is None:
@@ -76,11 +100,13 @@ class FixedGridODESolver(metaclass=abc.ABCMeta):
             if grid_constructor is None:
                 self.grid_constructor = self._grid_constructor_from_step_size(step_size)
             else:
-                raise ValueError("step_size and grid_constructor are mutually exclusive arguments.")
+                raise ValueError(
+                    "step_size and grid_constructor are mutually exclusive arguments."
+                )
 
     @classmethod
     def valid_callbacks(cls):
-        return {'callback_step'}
+        return {"callback_step"}
 
     @staticmethod
     def _grid_constructor_from_step_size(step_size):
@@ -89,10 +115,14 @@ class FixedGridODESolver(metaclass=abc.ABCMeta):
             end_time = t[-1]
 
             niters = torch.ceil((end_time - start_time) / step_size + 1).item()
-            t_infer = torch.arange(0, niters, dtype=t.dtype, device=t.device) * step_size + start_time
+            t_infer = (
+                    torch.arange(0, niters, dtype=t.dtype, device=t.device) * step_size
+                    + start_time
+            )
             t_infer[-1] = t[-1]
 
             return t_infer
+
         return _grid_constructor
 
     @abc.abstractmethod
@@ -100,15 +130,22 @@ class FixedGridODESolver(metaclass=abc.ABCMeta):
         pass
 
     def integrate(self, t):
+        event_index = 0
         time_grid = self.grid_constructor(self.func, self.y0, t)
+        time_grid = torch.concat([time_grid, self.jump_t], dim=0)
+        time_grid, _ = time_grid.sort()
+        time_grid = time_grid.unique()
         assert time_grid[0] == t[0] and time_grid[-1] == t[-1]
 
-        solution = torch.empty(len(t), *self.y0.shape, dtype=self.y0.dtype, device=self.y0.device)
+        solution = torch.empty(
+            len(t), *self.y0.shape, dtype=self.y0.dtype, device=self.y0.device
+        )
         solution[0] = self.y0
 
         j = 1
         y0 = self.y0
         for t0, t1 in zip(time_grid[:-1], time_grid[1:]):
+            dJ = torch.zeros_like(y0)
             dt = t1 - t0
             self.func.callback_step(t0, y0, dt)
             dy, f0 = self._step_func(self.func, t0, dt, t1, y0)
@@ -116,19 +153,40 @@ class FixedGridODESolver(metaclass=abc.ABCMeta):
 
             while j < len(t) and t1 >= t[j]:
                 if self.interp == "linear":
-                    solution[j] = self._linear_interp(t0, t1, y0, y1, t[j])
+                    dJ1, event_index = self._linear_interp_events(
+                        t0, t1, y0, y1, t[j], event_index
+                    )
+                    dJ += dJ1
+                    solution[j] = self._linear_interp(t0, t1, y0, y1, t[j]) + dJ
+                    dJ2, event_index = self._linear_interp_events(
+                        t0, t1, y0, y1, t1, event_index
+                    )
+                    dJ += dJ2
                 elif self.interp == "cubic":
                     f1 = self.func(t1, y1)
-                    solution[j] = self._cubic_hermite_interp(t0, y0, f0, t1, y1, f1, t[j])
+                    dJ1, event_index = self._cubic_hermite_events(
+                        t0, y0, f0, t1, y1, f1, t[j], event_index
+                    )
+                    dJ += dJ1
+                    solution[j] = (
+                            self._cubic_hermite_interp(t0, y0, f0, t1, y1, f1, t[j]) + dJ
+                    )
+                    dJ2, event_index = self._cubic_hermite_events(
+                        t0, y0, f0, t1, y1, f1, t1, event_index
+                    )
+                    dJ += dJ2
+
                 else:
                     raise ValueError(f"Unknown interpolation method {self.interp}")
                 j += 1
-            y0 = y1
+            y0 = y1 + dJ
 
         return solution
 
     def integrate_until_event(self, t0, event_fn):
-        assert self.step_size is not None, "Event handling for fixed step solvers currently requires `step_size` to be provided in options."
+        assert (
+                self.step_size is not None
+        ), "Event handling for fixed step solvers currently requires `step_size` to be provided in options."
 
         t0 = t0.type_as(self.y0.abs())
         y0 = self.y0
@@ -150,10 +208,14 @@ class FixedGridODESolver(metaclass=abc.ABCMeta):
                     interp_fn = lambda t: self._linear_interp(t0, t1, y0, y1, t)
                 elif self.interp == "cubic":
                     f1 = self.func(t1, y1)
-                    interp_fn = lambda t: self._cubic_hermite_interp(t0, y0, f0, t1, y1, f1, t)
+                    interp_fn = lambda t: self._cubic_hermite_interp(
+                        t0, y0, f0, t1, y1, f1, t
+                    )
                 else:
                     raise ValueError(f"Unknown interpolation method {self.interp}")
-                event_time, y1 = find_event(interp_fn, sign0, t0, t1, event_fn, float(self.atol))
+                event_time, y1 = find_event(
+                    interp_fn, sign0, t0, t1, event_fn, float(self.atol)
+                )
                 break
             else:
                 t0, y0 = t1, y1
@@ -169,7 +231,7 @@ class FixedGridODESolver(metaclass=abc.ABCMeta):
         h10 = h * (1 - h) * (1 - h)
         h01 = h * h * (3 - 2 * h)
         h11 = h * h * (h - 1)
-        dt = (t1 - t0)
+        dt = t1 - t0
         return h00 * y0 + h10 * dt * f0 + h01 * y1 + h11 * dt * f1
 
     def _linear_interp(self, t0, t1, y0, y1, t):
@@ -179,3 +241,27 @@ class FixedGridODESolver(metaclass=abc.ABCMeta):
             return y1
         slope = (t - t0) / (t1 - t0)
         return y0 + slope * (y1 - y0)
+
+    def _linear_interp_events(self, t0, t1, y0, y1, t_lim, e_index):
+        dJ = torch.zeros_like(y0)
+        while e_index < len(self.jump_t) and t_lim >= self.jump_t[e_index]:
+            y_event = self._linear_interp(t0, t1, y0, y1, self.jump_t[e_index])
+            h = self.jump(self.jump_t[e_index], y_event)
+            dN = self.events[:, e_index]
+            shape = list(dN.shape) + [1 for _ in range(len(h.shape) - len(dN.shape))]
+            dJ += h * dN.reshape(shape)
+            e_index += 1
+        return dJ, e_index
+
+    def _cubic_hermite_events(self, t0, y0, f0, t1, y1, f1, t_lim, e_index):
+        dJ = torch.zeros_like(y0)
+        while e_index < len(self.jump_t) and t_lim >= self.jump_t[e_index]:
+            y_event = self._cubic_hermite_interp(
+                t0, y0, f0, t1, y1, f1, self.jump_t[e_index]
+            )
+            h = self.jump(self.jump_t[e_index], y_event)
+            dN = self.events[:, e_index]
+            shape = list(dN.shape) + [1 for _ in range(len(h.shape) - len(dN.shape))]
+            dJ += h * dN.reshape(shape)
+            e_index += 1
+        return dJ, e_index
